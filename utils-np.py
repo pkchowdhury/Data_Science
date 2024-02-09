3a0800c63d3fae705b08d2700e094e01a1ffce22
Pallab Chowdhury <chowdhurypall95@gmail.com>
2024-02-09 16:27:19 +0600

    Kaggle Notebook | utils | code updated

--- utils-np.py
+++ utils-np.py
@@ -20,33 +20,41 @@ def dummy_loader(model_path):
     W = backbone.get_weights()
     return W
 
-def image_to_array(arrays):
+def image_to_array(filenames, size, channel):
     '''
-    Converting RGB images represented as ndarrays to a single numpy array.
+    Converting RGB images to numpy arrays.
     
     Input
     ----------
-        arrays: an iterable of ndarrays representing images
+        filenames: an iterable of the path of image files
+        size: the output size (height == width) of image. 
+              Processed through PIL.Image.NEAREST
+        channel: number of image channels, e.g. channel=3 for RGB.
         
     Output
     ----------
-        An array with shape = (num_arrays, height, width, channels)
+        An array with shape = (filenum, size, size, channel)
         
     '''
     
-    # number of arrays
-    num_arrays = len(arrays)
-    
-    # assume all arrays have the same shape
-    size, _, channel = arrays[0].shape
+    # number of files
+    L = len(filenames)
     
     # allocation
-    out = np.empty((num_arrays, size, size, channel))
+    out = np.empty((L, size, size, channel))
     
-    for i, arr in enumerate(arrays):
-        out[i] = arr
-        
-    return out
+    # loop over filenames
+    if channel == 1:
+        for i, name in enumerate(filenames):
+            with Image.open(name) as pixio:
+                pix = pixio.resize((size, size), Image.NEAREST)
+                out[i, ..., 0] = np.array(pix)
+    else:
+        for i, name in enumerate(filenames):
+            with Image.open(name) as pixio:
+                pix = pixio.resize((size, size), Image.NEAREST)
+                out[i, ...] = np.array(pix)[..., :channel]
+    return out[:, ::-1, ...]
 
 def shuffle_ind(L):
     '''
